7d06f3773fc5d1d7fda311109921ef9bc87dedbf
2429859206@qq.com <2429859206@qq.com>
2023-12-14 22:44:52 +0800

    阿里云 文件上传

---
+++ server/src/main/java/com/fxz/controller/common/CommonController.java
@@ -0,0 +1,55 @@
+package com.fxz.controller.common;
+
+import com.fxz.constant.MessageConstant;
+import com.fxz.result.Result;
+import com.fxz.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @projectName: take-out-system
+ * @package: com.fxz.controller.admin.common
+ * @className: CommonController
+ * @author: fxz
+ * @description: TODO
+ * @date: 2023/12/14 20:59
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传",file);
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件名的后缀   dfdfdf.png
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        }catch (IOException ex){
+            log.error("文件上传失败：{}",ex);
+        }
+
+        return  Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
