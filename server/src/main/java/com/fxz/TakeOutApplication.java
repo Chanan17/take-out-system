57559acfb93af290f51164650d85e968463e10e8
2429859206@qq.com <2429859206@qq.com>
2023-12-22 21:53:03 +0800

    开启缓存注解

--- server/src/main/java/com/fxz/TakeOutApplication.java
+++ server/src/main/java/com/fxz/TakeOutApplication.java
@@ -3,11 +3,13 @@ package com.fxz;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
+@EnableCaching//开发缓存注解功能
 public class TakeOutApplication {
     public static void main(String[] args) {
         SpringApplication.run(TakeOutApplication.class, args);
